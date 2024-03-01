5fb30d0b38afcd933a433ba3c38435d499b471a1
Natalia Voropai <natalia.voropai@uvoteam.com>
2024-03-01 22:49:53 +0100

    Added decrypt.py to milestone2

---
+++ milestone2/decrypt.py
@@ -0,0 +1,20 @@
+def decrypt() -> str:
+    message = input('Enter a message to decrypt ')
+    key = ''
+    while key.isdigit() is not True:
+        key = input('Enter int as key: ')
+    key = int(key)
+    abc = 'abcdefghijklmnopqrstuvwxyz'
+    new_message = ''
+    for i in message:
+        if i.lower() in abc:
+            if i.isupper():
+                new_message += abc[(abc.find(i.lower())-key) % 26].upper()
+            else:
+                new_message += abc[(abc.find(i)-key) % 26]
+        else:
+            new_message += i
+    return new_message
+
+
+print(decrypt())
